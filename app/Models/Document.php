701038f83068da629f4efaec46c11bf6ba55137d
reyginsusas <sreygin@gmail.com>
2026-08-13 15:24:40 +0800

    Added request classes for store and update, also added API route file

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Document extends Model
 {
-    //
+    protected $fillable = [
+        'tracking_code',
+        'title',
+        'status'
+    ];
 }
