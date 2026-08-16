cf94ccb7140656125ab8664cdaa81e80624968e9
reyginsusas <sreygin@gmail.com>
2026-08-16 15:42:14 +0800

    Upload File UI Version 2

--- resources/views/document/create.blade.php
+++ resources/views/document/create.blade.php
@@ -21,10 +21,11 @@
             <input type="file" name="file_input" id="file-input" accept=".pdf, docx, doc, .txt" hidden>
             <label for="file-input" class="upload-label">
                 <img src="/images/upload.png" alt="upload icon" class="upload-icon">
-                <button class="browse-btn">Browse</button>
+                <span class="browse-btn">Browse</span>
                 <p class="upload-guide">Drop a file here</p>
-                <p class="upload-guide"><span class="required-sign">*</span>File supported: .pdf, .docx, .doc, .txt</p>
+                <small class="upload-guide"><span class="required-sign">*</span>File supported: .pdf, .docx, .doc, .txt</small>
             </label>
+            <button type="submit" class="submit-btn" disabled>Upload File</button>
         </form>
     </main>
 </body>
